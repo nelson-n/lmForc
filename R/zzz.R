1329a00e67d1336e567b910414ffd82884cb3f0c
Nelson Rayl <68890648+lucius-verus-fan@users.noreply.github.com>
2021-10-09 14:05:58 -0700

    Delete R directory

--- R/zzz.R
+++
@@ -1,9 +0,0 @@
-
-#===============================================================================
-# Welcome to lmForc
-#===============================================================================
-
-# .onAttach <- function(libname, pkgname) {
-#   packageStartupMessage("Welcome to lmForc, the package for all of your linear
-# model forecasting needs. Ceci n'est pas une fourchette.")
-# }
